a76c4fc2cc50e9a4418055b01d6595546f7ac7f8
msunder <mohithsunder@gmail.com>
2026-03-18 12:03:35 +0530

    fix: clean package.json

--- src/server.js
+++ src/server.js
@@ -5,6 +5,11 @@ const app = express();
 
 app.use("/api", api);   // ⭐ important
 
+
+// app.listen(3002, () => {
+//   console.log("Server running on port 3002");
+// });
+
 const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, "0.0.0.0", () => {
